f6946e20bbde96d80fadf9b9d71acbe8af6f6edf
Filip Elander <felander@kth.se>
2022-02-05 16:40:14 +0100

    The finished Stack

--- Stack.py
+++ Stack.py
@@ -1,1 +1,48 @@
-# This is a file 
+
+""" 
+a custom stack, uses the python list to create a stack. 
+
+Push: to add an element to the top of the stack.
+Pop: to retrieve the element at the top of the stack.
+Size: to get the size of the stack.
+Empty: to check if the stack is empty.
+Peek: to get the value of the element at the top of the stack.
+
+"""
+
+
+
+class Stack:
+	# Init
+	def __init__(self):
+		#the list
+		self.elements = []
+
+
+
+	# Pushes a element to the top of the stack	
+	def push(self,data):
+	
+		self.elements.append(data)
+	#pops the element on the top of the list (includes error handle if empty) 	
+	def pull(self):
+		if self.elements:
+			return self.elements.pop()
+		else:
+			return None	
+
+	# returns the size of the stack 		
+	def size(self):
+		return len(self.elements)
+
+	# check if stack is empty	
+	def empty(self):
+		return True if self.size() == 0 else False
+
+	# peeks at the elemnt of the top of the stack 
+	def peek(self):
+		return self.elements[-1]
+
+
+
+
